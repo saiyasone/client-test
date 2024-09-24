1129888803411e1d0ef3dd19ad94467ba7ac3c70
lien teng <olienteng895@gmail.com>
2024-09-24 15:03:19 +0700

    changes

--- src/components/dialog/DialogOneTimeLink.tsx
+++ src/components/dialog/DialogOneTimeLink.tsx
@@ -415,7 +415,7 @@ const DialogOneTimeLink = (props) => {
                                 }}
                               >
                                 <IconFolderContainer>
-                                  {(item?.total_size || item?.totalSize) > 0 ? (
+                                  {(item?.total_size || item.totalSize!) > 0 ? (
                                     <FolderNotEmptyIcon />
                                   ) : (
                                     <FolderEmptyIcon />
@@ -428,7 +428,7 @@ const DialogOneTimeLink = (props) => {
                               </div>
                               <Typography>
                                 {convertBytetoMBandGB(
-                                  item?.total_size || item?.totalSize,
+                                  item?.total_size || item.totalSize!,
                                 )}
                               </Typography>
                             </Box>
@@ -497,7 +497,7 @@ const DialogOneTimeLink = (props) => {
                               </div>
                               <Typography>
                                 {convertBytetoMBandGB(
-                                  item?.size || item?.totalSize,
+                                  item?.size || item.totalSize!,
                                 )}
                               </Typography>
                             </Box>
